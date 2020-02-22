8acfa849207d9253dbb2a6eb6d159df588e021d6
Hanae05 <chacopipi0516@icloud.com>
2020-02-22 15:52:22 +0900

    正規表現

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const http = require('http');
 const fs = require('fs');
 
-var server = http.createServer(
-    (request,response)=>{
-        fs.readFile('./index.html', 'UTF-8',
-        (error,data) => {
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(data);
-            response.end();
-        });
-    }
-);
+var server = http.createServer(getFromClient);
 
 server.listen(3000);
 console.log('Server start!');
+
+// ここまでメインプログラム＝＝＝＝＝＝
+
+
+// createServerの処理
+function getFromClient(reqest,response){
+    fs.readFile('./index.html', 'UTF-8',
+        (error,data) => {
+            var content = data.
+                replace(/dummy_title/g, 'タイトルです').
+                replace(/dummy_content/g, 'これがコンテンツです。');
+            
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(content);
+            response.end();
+        }
+    );
+}
+
+
+        
